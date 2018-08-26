26414ba1e4393a0225e8318ce4fc9269b0f32c8e
sumit-hanumante <sumithanumanteblog@gmail.com>
2018-08-27 01:54:04 +0530

    initial commit

---
+++ sorting-alorithms/src/BubbleSort.java
@@ -0,0 +1,63 @@
+package src;
+
+import java.util.Arrays;
+
+/**
+ * Created by sumit .
+ *
+ * implementation of bubble sort
+ * in ascending and descending orders
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] a = {23,12,1,2,3,24,54,126,76};
+        System.out.println("in descending Order");
+        sortInAscedning(a);
+        System.out.println(Arrays.toString(a));
+        sortInDescending(a);
+        System.out.println("in descending Order");
+        System.out.println(Arrays.toString(a));
+    }
+
+    private static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+     private static void sortInAscedning(int arr[]){
+
+         boolean isSorted = false;
+         int lastSortedIndex = arr.length-1;
+
+         while (!isSorted){
+             isSorted = true;
+             for (int i = 0; i<lastSortedIndex; i ++){
+                 if (arr[i]> arr[i+1]){
+                     isSorted = false;
+                     swap(arr, i, i + 1);
+                 }
+             }
+             lastSortedIndex--;
+         }
+     }
+
+    private static void sortInDescending(int arr[]){
+
+        boolean isSorted = false;
+        int lastSortedIndex = arr.length-1;
+
+        while (!isSorted){
+            isSorted = true;
+            for (int i = 0; i<lastSortedIndex; i ++){
+                if (arr[i]<arr[i+1]){
+                    isSorted = false;
+                    swap(arr, i, i + 1);
+                }
+            }
+            lastSortedIndex--;
+        }
+    }
+}
